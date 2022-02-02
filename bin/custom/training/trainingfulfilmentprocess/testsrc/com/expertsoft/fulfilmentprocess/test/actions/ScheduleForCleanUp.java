c82e59b3906a3d84e44a98094267cea6de704406
DONTWARRYIMAFIME <ulas.kastsiukovich@expert-soft.by>
2022-02-02 19:34:57 +0300

    Task 1: Setup

---
+++ bin/custom/training/trainingfulfilmentprocess/testsrc/com/expertsoft/fulfilmentprocess/test/actions/ScheduleForCleanUp.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.expertsoft.fulfilmentprocess.test.actions;
+
+import com.expertsoft.fulfilmentprocess.actions.order.ScheduleForCleanUpAction;
+
+
+/**
+ * Test counterpart for {@link ScheduleForCleanUpAction}
+ */
+public class ScheduleForCleanUp extends TestActionTemp
+{
+	//EMPTY
+}
